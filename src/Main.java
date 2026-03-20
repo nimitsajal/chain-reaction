8e92c1328d3f8f028f0288a169428e32e6a64723
nimitsajal <nimitsajal99@gmail.com>
2026-03-21 00:17:22 +0530

    feat: built basic input and increment values
    - added input for the size of the grid
    - added input for row and column for the position of the move
    - increment calue of the selected position and render the grid again
    - added very basic explosion logic
    - not added any failure cases like of the input is out of bounds
    - not added the recursive calls to update and render each time any value gets updated

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int size = 10;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the Grid: ");
+        int size = sc.nextInt();
+//        int size = 10;
+
         int[][] defaultGrid = getDefaultGrid(size);
         printGrid(defaultGrid, size);
+
+        while(true) {
+            System.out.println("Enter Row and Column: ");
+            int r = sc.nextInt() - 1;
+            int c = sc.nextInt() - 1;
+
+            int[][] grid = defaultGrid;
+
+            grid[r][c] += 1;
+            if (grid[r][c] == 3) {
+                grid[r][c] = 0;
+                grid[r-1][c] += 1;
+                grid[r][c-1] += 1;
+                grid[r+1][c] += 1;
+                grid[r][c+1] += 1;
+            }
+            printGrid(grid, size);
+        }
     }
 
     public static int[][] getDefaultGrid(int size) {
 
-        int[][] grid = new int[size + 1][size];
+        int[][] grid = new int[size][size];
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -27,12 +52,12 @@ public class Main {
 
     private static void printTopRowWithBorder(int size) {
         System.out.print("     ");
-        for (int k = 1; k < size + 1; k++) {
+        for (int k = 1; k <= size; k++) {
             System.out.print(printNum(k) + " ");
         }
         System.out.println();
         System.out.print("   ┌─");
-        for (int k = 1; k < size + 1; k++) {
+        for (int k = 1; k <= size; k++) {
             System.out.print("───");
         }
         System.out.print("─┐");
@@ -52,10 +77,9 @@ public class Main {
         }
     }
 
-
     private static void printBottomBorder(int size) {
         System.out.print("   └─");
-        for (int k = 1; k < size + 1; k++) {
+        for (int k = 1; k <= size; k++) {
             System.out.print("───");
         }
         System.out.print("─┘");
