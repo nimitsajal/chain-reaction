3d56bbc6199654e44e99579522268f1ae9947d29
nimitsajal <40919943+nimitsajal@users.noreply.github.com>
2026-03-20 22:42:34 +0530

    refactor: clean up code structure and improve readability (#1)
    * removed unused code and separated different sections of print to separate methods
    
    * add coderabbit config back to the README.md as it works in the PRs
    
    * refactor: update code to resolve review comments
    
    - replaced printf with print
    - limit the size of rows to just size in the getDefaultGrid method
    - add println in the end of last border row
    - updated order of placement of methods to represent their actual method calls order

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,11 @@ public class Main {
         printGrid(defaultGrid, size);
     }
 
-    public static void addSeparation() {
-        System.out.println("                                ");
-        System.out.println("                                ");
-        System.out.println("                                ");
-        System.out.println("                                ");
-        System.out.println("                                ");
-    }
-
     public static int[][] getDefaultGrid(int size) {
 
         int[][] grid = new int[size + 1][size];
 
-        for (int i = 0; i < size + 1; i++) {
+        for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 grid[i][j] = 0;
             }
@@ -28,32 +20,46 @@ public class Main {
     }
 
     public static void printGrid(int[][] grid, int size) {
-        for (int i = 0; i < size + 1; i++) {
-            if (i > 0) { // actual grid
-                System.out.print(printNum(i) + " │ ");
-                for (int j = 0; j < size; j++) {
-                    System.out.printf(printNum(grid[i][j]) + " ");
-                }
-                System.out.println(" │ ");
-            } else { // top row for the column numbers
-                System.out.print("     ");
-                for (int k = 1; k < size + 1; k++) {
-                    System.out.printf(printNum(k) + " ");
-                }
-                System.out.println();
-                System.out.print("   ┌─");
-                for (int k = 1; k < size + 1; k++) {
-                    System.out.print("───");
-                }
-                System.out.print("─┐");
-                System.out.println();
-            }
+        printTopRowWithBorder(size);
+        printMainGrid(grid, size);
+        printBottomBorder(size);
+    }
+
+    private static void printTopRowWithBorder(int size) {
+        System.out.print("     ");
+        for (int k = 1; k < size + 1; k++) {
+            System.out.print(printNum(k) + " ");
         }
+        System.out.println();
+        System.out.print("   ┌─");
+        for (int k = 1; k < size + 1; k++) {
+            System.out.print("───");
+        }
+        System.out.print("─┐");
+        System.out.println();
+    }
+
+    private static void printMainGrid(int[][] grid, int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(printNum(i+1) + " │ ");
+            for (int j = 0; j < size; j++) {
+                System.out.print(printNum(grid[i][j]) + " ");
+
+                // NOTE: to check the position of each element
+//                System.out.print(i + String.valueOf(j) + " ");
+            }
+            System.out.println(" │ ");
+        }
+    }
+
+
+    private static void printBottomBorder(int size) {
         System.out.print("   └─");
         for (int k = 1; k < size + 1; k++) {
             System.out.print("───");
         }
         System.out.print("─┘");
+        System.out.println();
     }
 
     public static String printNum(int num) {
