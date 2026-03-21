f1d080a59ada28cf571084a215944f8fa31948e2
nimitsajal <nimitsajal99@gmail.com>
2026-03-22 00:34:43 +0530

    feat: add basic validation check in the setters of the move class to avoid -ve values
    - throws IllegalArgumentException if any -ve values are set to the row of column of move
    - update variable and method names for coll to col

--- src/Move.java
+++ src/Move.java
@@ -6,15 +6,21 @@ public class Move {
         return rowPos;
     }
 
-    public void setRowPos(int rowPos) {
-        this.rowPos = rowPos;
-    }
-
     public int getCollPos() {
         return collPos;
     }
 
+    public void setRowPos(int rowPos) {
+        if (rowPos <= 0) {
+            throw new IllegalArgumentException("rowPos must be >= 1");
+        }
+        this.rowPos = rowPos;
+    }
+
     public void setCollPos(int collPos) {
+        if (collPos <= 0) {
+            throw new IllegalArgumentException("collPos must be >= 1");
+        }
         this.collPos = collPos;
     }
 }
